ae7580cdf28e96984a40528560e4ebb12e287713
Marc Mendoza <777marc@gmail.com>
2020-01-30 20:37:37 -0500

    auth routes

---
+++ ui/src/containers/app/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './auth/auth';
+
+const PrivateRoute = function ({component: Component, ...rest}) {
+
+  // temp bug fix
+  let authed = isAuthenticated();
+
+
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+      />
+    )
+}
+
+export default PrivateRoute;
