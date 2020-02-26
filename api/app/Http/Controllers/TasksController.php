1ffc170ea45d707b4cf0328308d1990e259c842d
Marc Mendoza <777marc@gmail.com>
2020-02-25 20:29:31 -0500

    adding tasks

---
+++ api/app/Http/Controllers/TasksController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+
+    /**
+     * Story Controller Construct
+     */
+    public function __construct() {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::where('organization_id', $this->user->organization_id)
+                     ->get();
+        return response($tasks, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ api/database/migrations/2020_02_22_233717_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->integer('estimate');
+            $table->boolean('complete')->default(false);
+            $table->integer('owner');
+            $table->integer('story_id')->nullable();
+            $table->integer('percent_complete')->nullable();
+            $table->integer('organization_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
