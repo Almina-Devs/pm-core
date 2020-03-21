a0e7395366fd8838770d9c593b5f50137f523ac1
Marc Mendoza <marcmendoza@marcs-mac-mini.lan>
2020-03-21 14:59:27 -0400

    rtl setup

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders nav page', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('Dashboard');
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+
+    beforeEach( async () => {
+
+    });
+
+    test('initial test', () => {
+        expect(1).toBe(1);
+    })
+
 });
